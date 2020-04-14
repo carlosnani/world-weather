b555764fb7680ea92fe7d312fe133911016cc23d
carlosnani <carlosnani80@gmail.com>
2020-04-14 17:20:54 -0300

    Atualização da interface e funcionaliade de pesquisa de cidadaes ou páis - tratamento de erro.

--- js/app.js
+++ js/app.js
@@ -1,53 +1,80 @@
+const urlApi =    'https://api.openweathermap.org/data/2.5/weather?q=';
+const key =       'a42c7c0336c9886770b59a955c25e282';
+const units =     '&units=metric&appid=';
+
+const submit =    document.querySelector('#submit');
+const inputCity = document.querySelector('#inputCity')
+
+let cidadeContainer = document.querySelector('.cidadeName');
+let tempContainer = document.querySelector('.tempWeather');
+let humidiContainer = document.querySelector('.humidityData');
+let maxContainer = document.querySelector('.tempMaxData');
+let minContainer = document.querySelector('.tempMinData');
+let pressureContainer = document.querySelector('.pressaoData');
+
+submit.addEventListener('click', function(e){
+    e.preventDefault();    
+    inputCity.innerHTML =''
+    weatherBalloon(inputCity.value);
+});
+
 function weatherBalloon(cityID) {
-  const key = 'a42c7c0336c9886770b59a955c25e282';
-  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&units=metric&appid=' + key)
+  fetch(urlApi + cityID + units + key)
     .then(function (resp) { return resp.json() })
     .then(function (data) {
-
       takeCityInfo(data); //This function take the city name
       takeCityTemp(data); //This function take the City temp
       takeCityHumidity(data);//This function take the City Humidity
       takeCityMax(data); // This function take the Max Temp
       takeCityMin(data); // This function take the Min Temp
       takeCitypressure(data); //This function take the pressure  
-      iconeWeather(data);    
+      iconeWeather(data); // This function take the Icon    
+      console.log(data)
     })
-    .catch(function () {
-      console.log('This come with the error from openweathermap');
+    .catch(function (err) {
+      console.log('This come with the error from openweathermap - ' + err);
+      cidadeContainer.innerHTML = '<span style="font-size:18px">This city or country does not exist. Please try again or see this <a target="_blank" href="https://en.wikipedia.org/wiki/List_of_towns_and_cities_with_100,000_or_more_inhabitants/cityname:_A">Link</a></span>'
+      tempContainer.innerHTML = '00'
+      humidiContainer.innerHTML = '00'
+      maxContainer.innerHTML = '00'
+      minContainer.innerHTML = '00'
+      pressureContainer.innerHTML = '00'
     });
 }
-function takeCityInfo(data) { // Return the City Name
-  let cidadeContainer = document.querySelector('.cidadeName');
-  let cidadeInfo = document.createTextNode(data.name);
-  cidadeContainer.appendChild(cidadeInfo);
-}
+
 function takeCityTemp(data) { // Return the City Temp
-  let tempContainer = document.querySelector('.tempWeather');
-  let tempInfo = document.createTextNode(Math.floor(data.main.temp) + 'ºc');
+  let tempInfo = document.createTextNode(Math.floor(data.main.temp));
+  tempContainer.innerHTML = '';
   tempContainer.appendChild(tempInfo);
 }
 
+function takeCityInfo(data) { // Return the City Name
+  let cidadeInfo = document.createTextNode(data.name);
+  cidadeContainer.innerHTML ='';
+  cidadeContainer.appendChild(cidadeInfo); 
+}
+
 function takeCityHumidity(data) { // Return the City humidity
-  let humidiContainer = document.querySelector('.humidityData');
-  let humidiInfo = document.createTextNode(Math.floor(data.main.humidity) + '%');
+  let humidiInfo = document.createTextNode(Math.floor(data.main.humidity));
+  humidiContainer.innerHTML = '';
   humidiContainer.appendChild(humidiInfo);
 }
 
 function takeCityMax(data) { // Return the City Tem Max
-  let MaxContainer = document.querySelector('.tempMaxData');
-  let MaxInfo = document.createTextNode(Math.floor(data.main.temp_max) + 'ºc');
-  MaxContainer.appendChild(MaxInfo);
+  let maxInfo = document.createTextNode(Math.floor(data.main.temp_max));
+  maxContainer.innerHTML = '';
+  maxContainer.appendChild(maxInfo);
 }
 
 function takeCityMin(data) { // Return the City Tem Min
-  let MinContainer = document.querySelector('.tempMinData');
-  let MinInfo = document.createTextNode(Math.floor(data.main.temp_min) + 'ºc');
-  MinContainer.appendChild(MinInfo);
+  let minInfo = document.createTextNode(Math.floor(data.main.temp_min));
+  minContainer.innerHTML = '';
+  minContainer.appendChild(minInfo);
 }
 
 function takeCitypressure(data) { // Return the City Tem Min
-  let pressureContainer = document.querySelector('.pressaoData');
   let pressureInfo = document.createTextNode(Math.floor(data.main.pressure));
+  pressureContainer.innerHTML = '';
   pressureContainer.appendChild(pressureInfo);
 }
 
@@ -56,11 +83,10 @@ function iconeWeather(data) {
   let icon = data.weather[0].icon;
   let path = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
   weatherIcon.src = path;
-  console.log(path);
 }
 
 window.onload = function () {
-  weatherBalloon(3451190);
+  weatherBalloon('Rio de Janeiro');
 }
 
 function showAtualData() { //This Function take and show the Date.
@@ -74,5 +100,5 @@ function showAtualData() { //This Function take and show the Date.
   let mesAtual = document.querySelector('.mes').innerHTML = monName[mes] + ' ';
   let anoAtual = document.querySelector('.ano').innerHTML = ano;
 }
-showAtualData();
+// showAtualData();
 
